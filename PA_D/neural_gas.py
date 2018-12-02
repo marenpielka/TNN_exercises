51f925c3ded1b0843918a77a36b5a7d273aca417
Christopher Schmidt <christopher.schmidt@posteo.de>
2018-12-02 01:20:05 +0100

    Mehr Doku und dynamische learning curve

--- PA_D/neural_gas.py
+++ PA_D/neural_gas.py
@@ -31,14 +31,18 @@ class NeuralGas:
 
         return min(self.cached_responses)
 
-    def apply_learning_rule(self, stimulus, t):
+    def apply_learning_rule(self, stimulus, learning_rate):
+        """
+        Updates the centers for a given stimulus.
+        :return:
+        """
         # Get a list of the indexes of the sorted neurons
         neuron_list = [x[0] for x in sorted(enumerate(self.cached_responses), key=lambda x: x[1])]
 
         # Calculate and apply delta C_j. The distance is the position in the sorted neuron list.
         for dist, index in enumerate(neuron_list):
             # Calculate the delta
-            delta = [self._learning_rate(t) * self._neighborhood_fcn(dist) * (s - c)
+            delta = [learning_rate * self._neighborhood_fcn(dist) * (s - c)
                      for s, c in zip(stimulus, self.centers[index])]
 
             # Apply it to the center
@@ -48,12 +52,19 @@ class NeuralGas:
     def _neighborhood_fcn(self, dist):
         return exp(-1 / 2 * (dist ** 2) / (self.s ** 2))
 
-    def _learning_rate(self, t):
-        return 0.01
-
 
 class MultiNeuralGas:
-    def __init__(self, partner_networks, dimensions, neurons):
+    def __init__(self, partner_networks, dimensions, neurons, lrate_max, lrate_min):
+        """
+
+        :param partner_networks: Number of partner networks.
+        :param dimensions: Dimensions of the patterns
+        :param neurons: Number of neurons to use per net
+        :param lrate_max: The learning rate in the beginning
+        :param lrate_min: The learning rate at the end
+        """
+        self.max = lrate_max
+        self.min = lrate_min
         self.partners = [NeuralGas(dimensions, neurons) for _ in range(partner_networks)]
 
     def train(self, patterns, iterations):
@@ -68,13 +79,14 @@ class MultiNeuralGas:
             random.shuffle(patterns)
 
             for stimulus in patterns:
-                self._training_step(stimulus, t)
+                self._training_step(stimulus, t, iterations)
 
-    def _training_step(self, stimulus, t):
+    def _training_step(self, stimulus, t, iterations):
         """
         Executes a single step of learning by processing a single pattern.
         :param stimulus:
         :param t: The current timestep
+        :param iterations: Total number of iterations. Needed for the dynamic learning rate.
         :return:
         """
 
@@ -86,7 +98,7 @@ class MultiNeuralGas:
 
         # Select the network with the minimal response and update its weights
         winner = min(responses, key=itemgetter(0))[1]
-        winner.apply_learning_rule(stimulus, t)
+        winner.apply_learning_rule(stimulus, self.learning_rate(t, iterations))
 
     def store_centers(self, filename):
         """
@@ -101,12 +113,15 @@ class MultiNeuralGas:
                     fp.write('  '.join([str(x) for x in center]))
                     fp.write('\n')
 
+    def learning_rate(self, t, iterations):
+        return self.max * ((self.max / self.min) ** (-t / iterations))
+
 
 def training_file():
     """
     Trains the network with the patterns from PA-D-train.dat
     """
-    mngas = MultiNeuralGas(4, 2, 20)
+    mngas = MultiNeuralGas(4, 2, 30, lrate_max=0.01, lrate_min=0.001)
     mngas.store_centers('initial.net')
 
     with open("PA-D-train.dat", 'r') as fp:
@@ -128,10 +143,10 @@ def training_random():
     """
     patterns = []
     patterns += [[random.uniform(0.8, 1), random.uniform(0.1, 0.25)] for _ in range(300)]
-    patterns += [[random.uniform(0.05, 0.45), random.uniform(0.5, 0.95)] for _ in range(300)]
+    patterns += [[random.uniform(0.05, 0.15), random.uniform(0.8, 0.95)] for _ in range(300)]
     patterns += [[random.uniform(0.6, 0.8), random.uniform(0.6, 0.8)] for _ in range(300)]
 
-    mngas = MultiNeuralGas(4, 2, 20)
+    mngas = MultiNeuralGas(4, 2, 30, lrate_max=0.01, lrate_min=0.001)
     mngas.store_centers('initial.net')
     mngas.train(patterns, 1000)
     mngas.store_centers('PA-D.net')
