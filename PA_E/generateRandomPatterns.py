9c2dbacb79d5335355e53f13a8a1f9c571fc424d
Christopher Schmidt <christopher.schmidt@posteo.de>
2018-12-15 16:23:50 +0100

    Parameter, PyCharm reformat, zufällige Pattern mit Mustern

--- PA_E/generateRandomPatterns.py
+++ PA_E/generateRandomPatterns.py
@@ -5,14 +5,19 @@ Created on Sat Dec 15 15:11:37 2018
 @author: Maren
 """
 
-import numpy as np
+import random
 
-numPatterns = 100
-dim = 9
+if __name__ == '__main__':
+    numPatterns = 1000
+    dim = 2
 
-patterns = np.random.rand(numPatterns, dim)
+    patterns = []
+    patterns += [[random.uniform(0.8, 1), random.uniform(0.1, 0.25)] for _ in range(300)]
+    patterns += [[random.uniform(0.05, 0.15), random.uniform(0.8, 0.95)] for _ in range(300)]
+    patterns += [[random.uniform(0.6, 0.8), random.uniform(0.6, 0.8)] for _ in range(300)]
 
-with open("training3.dat", "w") as fp:
-    for i in range(numPatterns):
-        fp.write((" ".join([str(num) for num in patterns[i]])))
-        fp.write("\n")
+    with open("training_2dim.dat", "w") as fp:
+        for pattern in patterns:
+            fp.write((" ".join([str(num) for num in pattern])))
+            fp.write("\n")
+
